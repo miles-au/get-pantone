de5526cdf9ed0fc29c8a34b9c5de8b9ffae6fa5a
Miles Au <miles.d.au@gmail.com>
2019-10-16 02:03:13 -0400

    Updates

---
+++ precache-manifest.e35d912b0edb92a8f508ced55be2338b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2a3fa756c750c49516c808be8ebb525",
+    "url": "/get-pantone/index.html"
+  },
+  {
+    "revision": "b75dedafad9c703bc0c4",
+    "url": "/get-pantone/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "1cf5ddbfe8341b50fa34",
+    "url": "/get-pantone/static/css/main.d2f3af59.chunk.css"
+  },
+  {
+    "revision": "b75dedafad9c703bc0c4",
+    "url": "/get-pantone/static/js/2.4a9f4cc8.chunk.js"
+  },
+  {
+    "revision": "1cf5ddbfe8341b50fa34",
+    "url": "/get-pantone/static/js/main.4ebb5789.chunk.js"
+  },
+  {
+    "revision": "c5abbe9d87255844980c",
+    "url": "/get-pantone/static/js/runtime-main.76d8eca7.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/get-pantone/precache-manifest.e35d912b0edb92a8f508ced55be2338b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/get-pantone/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
